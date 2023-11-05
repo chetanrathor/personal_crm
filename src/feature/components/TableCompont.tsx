e2203befc636135f3424cae7b44b24a911e4d0ff
chetan <chetan@gmail.com>
2023-11-05 15:48:55 +0530

    added icon above

--- src/feature/components/TableCompont.tsx
+++ src/feature/components/TableCompont.tsx
@@ -44,7 +44,9 @@ const TableCompont = () => {
                         key={row.name}
                         sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                     >
-                        <TableCell> {offset+(i+1)}</TableCell> 
+                        <TableCell> {offset+(i+1) }  {
+                                row.personalizeEmails.length ? <DoneAllIcon color='success'></DoneAllIcon> : <CloseIcon color='error'></CloseIcon>
+                            }</TableCell> 
                          <TableCell>
                            <Button variant='contained' color='primary' onClick={() => { handelSendEmailClick(row) }} >Send Email</Button>  <Button variant='contained' color='warning' onClick={() => { handelFollowUpClick(row) }}>Follow Up</Button>
                         </TableCell>
