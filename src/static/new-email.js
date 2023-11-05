1b5f2bb99707968ee4d745b00f9631a7044ef98b
chetan <chetan@gmail.com>
2023-11-05 15:41:54 +0530

    change in json

--- src/static/new-email.js
+++ src/static/new-email.js
@@ -6,7 +6,7 @@ Hello ,
 
 I hope this email finds you well. I am writing to express my strong interest in joining your esteemed organization as a Full Stack Developer. After researching your company's reputation for innovation and commitment to excellence, I am excited about the opportunity to contribute to your team.
     
-With over 1 year of experience in both front-end and back-end technologies, I believe I have the skills and expertise necessary to excel in this role. I have attached my resume to provide you with a comprehensive overview of my professional background, which includes hands-on experience in:
+With over 1.5 year of experience in both front-end and back-end technologies, I believe I have the skills and expertise necessary to excel in this role. I have attached my resume to provide you with a comprehensive overview of my professional background, which includes hands-on experience in:
     
 - Front-end development using HTML, CSS, and JavaScript,React Js, to create responsive and user-friendly web interfaces.
 - Back-end development with proficiency in languages such as JavaScript, as well as experience with server-side frameworks like Node ExpressJs & NestJs.
@@ -19,7 +19,7 @@ I am particularly drawn to your organization because of its reputation for foste
     
 I would welcome the opportunity to discuss how my qualifications and experiences align with the needs of your team further. Please feel free to contact me at +918821889586 or via email at chetan.rathore3899@gmail.com to schedule a convenient time for a discussion or interview.
 
-Thank you for considering my application. I am eager to learn more about your organization and how my skills can contribute to your continuedsuccess. 
+Thank you for considering my application. I am eager to learn more about your organization and how my skills can contribute to your continued success. 
 
 Sincerely,
 
